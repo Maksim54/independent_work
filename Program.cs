0af8ffb045fe95586a3013ecd1fad38a8760c565
Maksim54 <77334211+Maksim54@users.noreply.github.com>
2021-11-01 10:55:49 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,8 +8,85 @@ namespace Repitition_Lagunovski
 {
     class Program
     {
+        /*task 2 from exercise 6*/
+        /* Задан массив действительных чисел из N элементов (используйте генератор случайных чисел).
+        Определить количество элементов, значения которых находятся вне диапазона от -10 до +10.*/
         static void Main(string[] args)
         {
+            Random rnd = new Random();
+            //
+            Console.Write("N = ");
+            var N = int.Parse(Console.ReadLine()); //создается целочисленная переменная N
+            var number = new int[N];
+            var counting = 0;
+            for (var i = 0; i < N; i++)//массив с числами от 1 до 20
+            {
+                number[i] = rnd.Next(1, 20);
+                if (number[i] > -10 && number[i] < 10) counting++;
+            }
+            //
+            Console.WriteLine($"Count {counting}");//выдаёт нам количество элементов, значения которых находятся вне диапазона от -10 до +10.
+            Console.ReadLine();
+            //
+            //
+            /*task 3 from exercise 6*/
+            //
+            /*Дано: информация о каждом жителе города содержит следующие элементы:
+            а) фамилия, имя, отчество;
+            б) домашний адрес (улица, дом);
+            в) дата рождения.
+            Информация о жителе является элементом одного большого массива М[ ].
+            Требуется составить списки избирателей, живущих по заданному адресу в алфавитном порядке.
+            Примечание: Правом участия в выборах обладают люди не моложе 18 лет.*/
+            //
+            //фамилия,имя,отчество
+            string[] Mfull_name =
+            {
+                "Isaev Yaroslav Egorovich -", "Morozova Zlata Timurovna -", "Bogdanova Daria Ivanovna -", "Vasilyeva Lyubov Valerievna -"
+            };
+            //
+            //номер улицы
+            string[] Madress =
+            {
+                "- 1130 Small Street", "- 3620 Hanover Street", "- 4313 Jarvisville Road", "- 2770 Fieldcrest Road"
+            };
+            //
+            //день,месяц и год рождения каждого из людей введенных в списке
+            int[] Myear =
+            {
+                1990, 1984, 1999, 2005
+            };
+            int[] Mmonth =
+            {
+                12, 04, 9, 03
+            };
+            int[] Mday =
+            {
+                03, 12, 07, 25
+            };
+            //
+            int a = Myear[0];
+            int b = Myear[1];
+            int c = Myear[2];
+            int counting1 = 0;
+            //
+            //Добавляем примечание тому человеку,который родился позже 2003 года
+            for (int i = 0; i < Mfull_name.Length; i++)
+            {
+                //
+                if (Myear[counting1] < 2003)
+                {
+                    Array.Sort(Madress, Mfull_name);
+                    Console.WriteLine(Madress[counting1] + " - / - " + Mfull_name[counting1]);
+                }
+                else
+                {
+                    Console.WriteLine(Mfull_name[counting1] + "People who are at least 18 years of age have the right to vote.");
+                }
+                counting1++;
+                //
+            }
+            Console.ReadLine();
         }
     }
 }
